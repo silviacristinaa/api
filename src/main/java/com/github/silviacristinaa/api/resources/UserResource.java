cd221e4eef4dc6f851d62a5f754c8e666347f209
silviacristinaa <silviacristinaalexandre1@gmail.com>
2022-10-04 09:21:33 -0300

    [feature/03] - Endpoint findAll e create para User

--- src/main/java/com/github/silviacristinaa/api/resources/UserResource.java
+++ src/main/java/com/github/silviacristinaa/api/resources/UserResource.java
@@ -1,11 +1,18 @@
 package com.github.silviacristinaa.api.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.github.silviacristinaa.api.domain.dtos.UserDto;
 import com.github.silviacristinaa.api.services.UserService;
@@ -17,11 +24,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserResource {
 	
-	private final ModelMapper mapper;
+	private final ModelMapper modelMapper;
 	private final UserService userService; 
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDto> findById(@PathVariable Long id) {
-		return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDto.class));
+		return ResponseEntity.ok().body(modelMapper.map(userService.findById(id), UserDto.class));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<UserDto>> findAll() {
+		return ResponseEntity.ok().body(userService.findAll()
+				.stream().map(x -> modelMapper.map(x, UserDto.class)).collect(Collectors.toList()));
+	}
+	
+	@PostMapping
+	public ResponseEntity<UserDto> create(@RequestBody UserDto obj) {
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}").buildAndExpand(userService.create(obj).getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/github/silviacristinaa/api/services/UserService.java
+++ src/main/java/com/github/silviacristinaa/api/services/UserService.java
@@ -1,8 +1,13 @@
 package com.github.silviacristinaa.api.services;
 
+import java.util.List;
+
 import com.github.silviacristinaa.api.domain.User;
+import com.github.silviacristinaa.api.domain.dtos.UserDto;
 
 public interface UserService {
 
 	User findById(Long id);
+	List<User> findAll();
+	User create(UserDto obj);
 }
