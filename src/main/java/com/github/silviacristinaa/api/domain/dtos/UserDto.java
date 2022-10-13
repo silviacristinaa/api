767f0f155fc5aa4a99e19130805751093614d40f
silviacristinaa <silviacristinaalexandre1@gmail.com>
2022-10-13 11:39:44 -0300

    [feature/o9] - Validações User e UserDto

--- src/main/java/com/github/silviacristinaa/api/domain/dtos/UserDto.java
+++ src/main/java/com/github/silviacristinaa/api/domain/dtos/UserDto.java
@@ -1,6 +1,7 @@
 package com.github.silviacristinaa.api.domain.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -13,9 +14,11 @@ import lombok.Setter;
 public class UserDto {
 	
 	private Long id; 
+	@NotBlank
 	private String name; 
+	@NotBlank
 	private String email; 
-	
+	@NotBlank
 	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	private String password;
 }
